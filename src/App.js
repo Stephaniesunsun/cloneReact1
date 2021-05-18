88c9376ca7a80b220ef786b5526ee2931ec2be57
steph0530 <steph0530@github.com>
2021-05-18 08:38:37 +0800

    Change SASS to global style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import PageLayout from './components';
-
+import GlobalStyle from './globalStyle';
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <PageLayout />
     </div>
   );
